c8d72e983b1f7526e0d2e90824b3337ef7f0cc0f
sharon <108038695+sr0427@users.noreply.github.com>
2022-07-14 10:01:25 +0900

    グラフを動かしてみた]

--- plt.py
+++ plt.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-x = np.linspace(-10, 10, 101)				
-y = np.exp(-x**2 / 2) / np.sqrt(2 * np.pi)		
-
-plt.plot(x, y)			
-plt.show()		
- 
+  
+x = np.linspace(-10, 10, 101)				# xを計算する
+y = np.exp(-x**2 / 2) / np.sqrt(2 * np.pi)+5
+  
+plt.plot(x, y)
+plt.show()
